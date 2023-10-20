cec104cf682022df6534319b279ef531b6ae7ed7
Kyle <kyle.stephens@ucdenver.edu>
2023-10-19 18:19:06 -0600

    secret auction

---
+++ 100 days python/day9/secret_auction.py
@@ -0,0 +1,40 @@
+#making use of dictionaries and nesting we will create a secret auction program 
+from clear import clear
+logo = '''
+                         ___________
+                         \         /
+                          )_______(
+                          |"""""""|_.-._,.---------.,_.-._
+                          |       | | |               | | ''-.
+                          |       |_| |_             _| |_..-'
+                          |_______| '-' `'---------'` '-'
+                          )"""""""(
+                         /_________\\
+                       .-------------.
+                      /_______________\\
+'''
+print(logo)
+print("Welcome to the Secret Auction program")
+bids = {}
+bidding_finished = False
+
+def highest_bidder(bidding_record):
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+
+while not bidding_finished:
+    name = input("What is your name?")
+    price = input("What is your bid $")
+    bids[name] = price
+    should_continue = input("Are there any other bidders? Type 'yes' or 'no'")
+    if should_continue == 'no':
+        bidding_finished = True
+    elif should_continue == 'yes':
+        clear()
+
+
+
+
+
